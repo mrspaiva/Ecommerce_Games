79d696912f4398deb7e7305ef344e0919703cef4
Natalia <natyap28@hotmail.com>
2020-12-03 17:10:18 -0300

    create Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+import styles from './styles'
 
-export default function Home() {
-  return (
-    <View>
-    
+import ArrowIcon from '../../Components/arrowIcon'
+
+export default function Cart() {
+  const navigation = useNavigation()
+
+  function navigateToHome() {
+    navigation.navigate('Home')
+  }
+
+  return(
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.statusBar}>
+          <Text style={styles.title}>E-commerce Games</Text>
+        </View>
+
+        <TouchableOpacity style={styles.back} onPress={navigateToHome}>
+          <ArrowIcon />
+        </TouchableOpacity>
+
+        <Text style={styles.cartText}>CARRINHO</Text>
+
+        <View style={styles.product}>
+          <Text style={styles.productName}>PRODUTO</Text>
+          <Text style={styles.productDelete}>X</Text>
+        </View>
+        <View style={styles.product}>
+          <Text style={styles.productName}>PRODUTO</Text>
+          <Text style={styles.productDelete}>X</Text>
+        </View>
+        <View style={styles.product}>
+          <Text style={styles.productName}>PRODUTO</Text>
+          <Text style={styles.productDelete}>X</Text>
+        </View>
+
+        <View style={styles.subtotal}>
+          <Text style={styles.subtotalText}>Subtotal:</Text>
+          <Text style={styles.subtotalValue}>R$ 120</Text>
+        </View>
+
+        <View style={styles.frete}>
+          <Text style={styles.freteText}>Frete:</Text>
+          <Text style={styles.freteValue}>R$ 30</Text>
+        </View>
+
+        <View style={styles.total}>
+          <Text style={styles.totalText}>Total:</Text>
+          <Text style={styles.totalValue}>R$ 150</Text>
+        </View>
+
+        <TouchableOpacity style={styles.buyButton}>
+          <Text style={styles.buy}>Comprar</Text>
+        </TouchableOpacity>
     </View>
   )
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,110 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bcbcbc',
+  },
+
+  statusBar: {
+    height: '12%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#fafafa',
+    borderRadius: 16,
+    marginBottom: 20
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+
+  back: {
+    marginTop: 15,
+    marginLeft: 10
+  },
+
+  cartText: {
+    alignSelf: 'center',
+    fontSize: 24,
+    marginBottom: '20%',
+    marginTop: '8%'
+  },
+
+  product: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 30
+  },
+
+  productName: {
+    marginLeft: 20,
+    fontWeight: 'bold'
+  },
+
+  productDelete: {
+    marginRight: 20,
+    fontWeight: 'bold'
+  },
+
+  subtotal: {
+    marginTop: 35,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+  },
+
+  subtotalText: {
+    fontSize: 18
+  },
+
+  frete: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+    borderBottomColor: '#999'
+  },
+
+  freteText: {
+    fontSize: 18
+  },
+
+  total: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+    marginTop: 20,
+    fontWeight: 'bold',
+  },
+
+  totalText: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+
+  totalValue: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+
+  buyButton: {
+    backgroundColor: '#e04999',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    height: '8%',
+    width: '80%',
+    marginTop: 30,
+    borderRadius: 10
+  },
+
+  buy: {
+    color: '#fff',
+    fontSize: 20
+  }
+})
