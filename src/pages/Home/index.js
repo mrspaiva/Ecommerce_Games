1c0596be0cab1923670a435e6780abad74cb4c15
Natalia <natyap28@hotmail.com>
2020-12-03 12:24:45 -0300

    create Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text } from 'react-native';
+import { View, FlatList, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
 
 export default function Home() {
+  const navigation = useNavigation()
+
+  function navigateToCart() {
+    navigation.navigate('Cart')
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.header}>
+      <View style={styles.statusBar}>
         <Text style={styles.title}>E-commerce Games</Text>
       </View>
+
+      <TouchableOpacity style={styles.buttonCart} onPress={navigateToCart}>
+        <Text>CARRINHO</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.filterBy}>Filtrar por:</Text>
+
+        <View style={styles.filterList}>
+          <View style={styles.filter}>
+            <Text style={styles.filterTextAlfabetica}>  Ordem Alfabética</Text>
+          </View>
+          <View style={styles.filter}>
+            <Text style={styles.filterText}>Score</Text>
+          </View>
+          <View style={styles.filter}>
+            <Text style={styles.filterText}>Preço</Text>
+          </View>
+        </View>
+
+
+      <FlatList 
+      data={[1, 2, 3, 4, 5, 6]} 
+      style={styles.gameList} 
+      keyExtractor={game => String(game)}
+      showsVerticalScrollIndicator={false}
+      renderItem={() => (
+        <View style={styles.game}>
+        <Image style={styles.gameImg} source={require('../../assets/call-of-duty-wwii.png')} />
+        <Text style={styles.gameName}>Call Of Duty</Text>
+
+        <View style={styles.gameInfo}>
+          <Text style={styles.gamePrice}>R$ 80,00</Text>
+          <Text style={styles.gameScore}>Score: 100</Text>
+        </View>
+        
+        <TouchableOpacity style={styles.buyButton} onPress={() => {}}>
+          <Text style={styles.buy}>Comprar</Text>
+        </TouchableOpacity>
+      </View>
+      )}/>
+
     </View>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,26 +1,126 @@
 import { StyleSheet } from 'react-native'
-import Constants from 'expo-constants'
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    paddingTop: Constants.statusBarHeight + 8,
-    backgroundColor: '#c4c4c4'
+    backgroundColor: '#bcbcbc',
   },
 
-  header: {
-    height: '20%',
+  statusBar: {
+    height: '12%',
     width: '100%',
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#666',
-    borderRadius: 14
+    justifyContent: 'flex-end',
+    backgroundColor: '#fafafa',
+    borderRadius: 16,
+    marginBottom: 20
   },
 
   title: {
     fontSize: 30,
     fontWeight: 'bold',
     color: '#000'
+  },
+
+  buttonCart: {
+    backgroundColor:'#999',
+    width: 100,
+    alignSelf: 'flex-end'
+  },
+
+  cart: {
+    alignSelf: 'center',
+    marginBottom: 20,
+    width: 50,
+    height: 50,
+  },
+
+  filterBy: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 10
+  },
+
+  filterList: {
+    flexDirection: 'row'
+  },
+
+  filter: {
+    marginTop: 10,
+    marginHorizontal: 10,
+    backgroundColor: '#fafafa',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 90,
+    borderRadius: 10,
+  },
+  
+  filterText: {
+    flexDirection:'column',
+    fontWeight: 'bold', 
+    flexWrap: 'wrap',
+  },
+
+  filterTextAlfabetica: {
+    marginHorizontal: '13%',
+    fontWeight: 'bold', 
+    marginBottom: 8,
+  },
+
+  gameList: {
+    marginTop: 38,
+  },
+
+  game: {
+    marginHorizontal: 20,
+    marginBottom: 24,
+    padding: 20,
+    borderRadius: 18,
+    backgroundColor: '#fafafa',
+    justifyContent: 'space-around'
+  },
+
+  gameImg: {
+    width: 200,
+    height: 200, 
+    alignSelf: 'center',
+  },
+
+  gameName: {
+    marginTop: 20,
+    fontSize: 36,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  },
+
+  gameInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 40
+  },
+
+  gamePrice: {
+    fontSize: 20,
+  },
+
+  gameScore: {
+    fontSize: 20,
+  },
+
+  buyButton: {
+    backgroundColor: '#e02000',
+    marginTop: 20,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    height: 40,
+    width: '50%'
+  },
+
+  buy: {
+    color: '#fff',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 18
   }
 })
